f237f95344a503177f67e6440dbe1e05bcce45dd
Gabriel <gabriel.g2d2@gmail.com>
2024-12-04 16:22:30 -0300

    feat: add mock data and display chips in ListGoals component

--- web/src/pages/Home/template/components/ListGoals/index.tsx
+++ web/src/pages/Home/template/components/ListGoals/index.tsx
@@ -1,3 +1,5 @@
+import { Add, Check } from "@mui/icons-material";
+import { Chip, Stack } from "@mui/material";
 import { Container } from "./components/Container";
 import { Header, IHeaderProps } from "./components/Header";
 
@@ -5,10 +7,42 @@ export type IListGoalsProps = {
   header: IHeaderProps;
 };
 
+const LIST_MOCK = [
+  {
+    title: "Teste 1",
+    isCompleted: false,
+  },
+  {
+    title: "Teste 2",
+    isCompleted: true,
+  },
+  {
+    title: "Teste 3",
+    isCompleted: true,
+  },
+];
+
 export function ListGoals(props: IListGoalsProps) {
   return (
     <Container>
       <Header {...props.header} />
+
+      <Stack my={4} direction="row" gap={2}>
+        {LIST_MOCK.map((item) => (
+          <Chip
+            key={item.title}
+            label={item.title}
+            variant="outlined"
+            icon={item.isCompleted ? <Check color="success" /> : <Add />}
+            sx={{
+              border: "1px dashed #313131",
+              fontSize: "1rem",
+              padding: "0.5rem",
+              height: "2.3rem",
+            }}
+          />
+        ))}
+      </Stack>
     </Container>
   );
 }
